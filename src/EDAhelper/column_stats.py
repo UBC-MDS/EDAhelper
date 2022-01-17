777a4a16a37ae4bd572a2a4da4d1a8202caf4fab
Rowansiv <90011761+Rowansiv@users.noreply.github.com>
2022-01-17 13:15:46 -0800

    Update column_stats.py

--- src/EDAhelper/column_stats.py
+++ src/EDAhelper/column_stats.py
@@ -1,6 +1,9 @@
+import pandas as pd
+
 def column_stats(data, columns = None):
   """
-  Obtain summary statistics of column(s) including count, mean, median, mode, Q1, Q3, variance, standard deviation, correlation.... (add more if required) in table format.
+  Obtain summary statistics of column(s) including count, mean, median, mode, Q1, Q3, 
+  variance, standard deviation, correlation.... (add more if required) in table format.
 
   Parameters
   -------------
@@ -21,4 +24,7 @@ def column_stats(data, columns = None):
   >>> column_stats(df, columns = c('Date', PctPopulation', 'CrimeRatePerPop'))
   >>>
   """
-pass
+  statsdict = {'Column': [], 'Count': [], 'Mean': [], 'Median': [], 'Mode': [], 'Q1': [], 'Q3': [], 'Var': [], 'Stdev': [], 'Corr': []}
+  for column in columns:
+    statsdict['Column'].append(column)
+    statsdict['Count'].append(
