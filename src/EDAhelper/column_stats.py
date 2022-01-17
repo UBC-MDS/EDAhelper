cc17b84b580664f3f9472c8fc69946da3cf85813
Rowansiv <90011761+Rowansiv@users.noreply.github.com>
2022-01-17 12:38:29 -0800

    Updating docstring for column_stats function

--- src/EDAhelper/column_stats.py
+++ src/EDAhelper/column_stats.py
@@ -1,4 +1,4 @@
-def column_stats(data, column1, column2 = None, column3 = None, column4 = None):
+def column_stats(data, columns = None):
   """
   Obtain summary statistics of column(s) including count, mean, median, mode, Q1, Q3, variance, standard deviation, correlation.... (add more if required) in table format.
 
@@ -8,17 +8,8 @@ def column_stats(data, column1, column2 = None, column3 = None, column4 = None):
   data: 
            The data set from which columns will be selected
 
-  column1:
-            Column for which to obtain summary stats and correlation (if > 1 column arguments used)
-
-  column2: optional
-            Column for which to obtain summary stats and correlation
-
-  column3: optional
-            Column for which to obtain summary stats and correlation
-
-  column4: optional
-            Column for which to obtain summary stats and correlation
+  columns: string vector
+            Columns for which to obtain summary stats and correlation (if > 1 column arguments used)
 
   Returns
   -------------
@@ -27,7 +18,7 @@ def column_stats(data, column1, column2 = None, column3 = None, column4 = None):
 
   Examples
   -------------
-  >>>
+  >>> column_stats(df, columns = c('Date', PctPopulation', 'CrimeRatePerPop'))
   >>>
   """
 pass
