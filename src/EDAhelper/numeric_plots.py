7b55e3246e71c41f83da5f5972191601d9e3aa9c
Steven Leung <klsleung@student.ubc.ca>
2022-01-17 21:45:46 -0800

    tests: remove lab2-movies.json as it is no longer needed.

--- src/EDAhelper/numeric_plots.py
+++ src/EDAhelper/numeric_plots.py
@@ -2,38 +2,28 @@ import pandas as pd
 import altair as alt
 
 
-def numeric_plots(df, ncols=5):
+def numeric_plots(df):
     """
     Takes a dataframe and plot the possible pairs of the numeric columns using Altair, 
-    creating a matrix of correlation plots. The function conveniently breaks up the 
-    features in the dataframe into blocks of `ncols` so that they can be fit into the 
-    width of a page.
+    creating a matrix of correlation plots. The function will automatically avoid all 
+    non-numeric features.
 
     Parameters:
         df: pandas.dataframe): 
             a pandas dataframe
-        ncols: int
-            number of columns.  default = 5
 
     Returns:
-        splom: list
-        a list of Altair chart objects for the plots
+        splom: Altair chart object
+        the object for the plots
     """
 
     # Data validation
     if not isinstance(df, pd.DataFrame):
         raise TypeError("'df' should be of type 'pandas.DataFrame'.")
 
-    if not isinstance(ncols, int):
-        raise TypeError("'ncols' should be of type 'int'.")
+    numeric_cols = df.select_dtypes(include=['float64', 'int64'])
 
-    n_pages = int(df.shape[1] / ncols)
-    if (df.shape[1] % ncols > 0):
-        n_pages += 1
-
-    splom = [None] * n_pages
-
-    splom[0] = alt.Chart(df).mark_point(opacity=0.3, size=10).encode(
+    splom = alt.Chart(numeric_cols).mark_point(opacity=0.3, size=10).encode(
         alt.X(
             alt.repeat('row'),
             type='quantitative',
--- tests/test_numeric_plots.py
+++ tests/test_numeric_plots.py
@@ -4,29 +4,33 @@ import altair as alt
 from EDAhelper.numeric_plots import numeric_plots
 
 
-def numeric_cols():
+def df():
     df = pd.DataFrame({
         'A': [100, 142],
         'B': [94, 55],
         'C': [94, 68],
         'D': [3.86, 4.05],
-        'E': [34, 92]
+        'E': [34, 92],
+        'name': ['Jennifer', 'Rowan', 'Steven', 'Vera'] 
     })
     return df
 
 
 def test_numeric_plots():
 
-    actual = numeric_plots(numeric_cols())
+    actual = numeric_plots(df())
 
     # Check that the return value is a list of objects
     assert isinstance(
-        actual, list), "The return value should be a list of Altair plot objects."
+        actual, alt.vegalite.v4.api.RepeatChart), "The return value should be an Altair plot objects."
 
-    for i in range(0, len(actual)):
-        assert isinstance(
-            actual[i], alt.vegalite.v4.api.RepeatChart), "The return list should contain objects of Altair plots."
+#    for i in range(0, len(actual)):
+#        assert isinstance(
+#            actual[i], alt.vegalite.v4.api.RepeatChart), "The return list should contain objects of Altair plots."
 
+    n_numeric_cols = 5
+
+    assert actual
 
 def test_numeric_plots_error():
     """Check TypeError raised when any of the argument type is wrong."""
@@ -34,6 +38,3 @@ def test_numeric_plots_error():
     list_object = ["Python", "is", "a", "language"]
     with pytest.raises(TypeError):
         numeric_plots(list_object)
-
-    with pytest.raises(TypeError):
-        numeric_plots(numeric_cols(), list_object)
