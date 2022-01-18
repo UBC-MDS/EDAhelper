26c2a80cf533f20b692558bc4b8c6e6638454421
Rowansiv <90011761+Rowansiv@users.noreply.github.com>
2022-01-17 17:20:53 -0800

    Writing first draft of code for column_stats

--- src/EDAhelper/column_stats.py
+++ src/EDAhelper/column_stats.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 def column_stats(data, columns = None):
-  """
+    """
   Obtain summary statistics of column(s) including count, mean, median, mode, Q1, Q3, 
   variance, standard deviation, correlation.... (add more if required) in table format.
 
   Parameters
   -------------
 
-  data: 
+  data: array_like
            The data set from which columns will be selected
 
   columns: string vector
@@ -24,7 +24,26 @@ def column_stats(data, columns = None):
   >>> column_stats(df, columns = c('Date', PctPopulation', 'CrimeRatePerPop'))
   >>>
   """
-  statsdict = {'Column': [], 'Count': [], 'Mean': [], 'Median': [], 'Mode': [], 'Q1': [], 'Q3': [], 'Var': [], 'Stdev': [], 'Corr': []}
-  for column in columns:
-    statsdict['Column'].append(column)
-    statsdict['Count'].append(
+    
+      statsdict = {'Column': [], 'Count': [], 'Mean': [], 'Median': [], 'Mode': [], 'Q1': [], 'Q3': [], 'Var': [], 'Stdev': []}
+    for column in columns:
+        statsdict['Column'].append(column)
+        statsdict['Count'].append(round(float(data[column].describe().loc['count']), 3))
+        statsdict['Mean'].append(round(float(data[column].describe().loc['mean']), 3))
+        statsdict['Median'].append(round(float(data[column].describe().loc['50%']), 3))
+        statsdict['Mode'].append(round(float(data[column].mode()), 3))
+        statsdict['Q1'].append(round(float(data[column].describe().loc['25%']), 3))
+        statsdict['Q3'].append(round(float(data[column].describe().loc['75%']), 3))
+        statsdict['Var'].append(round(data[column].var(), 3))
+        statsdict['Stdev'].append(round(data[column].std(), 3))
+    return pd.DataFrame.from_dict(statsdict)
+
+    cols = []
+    for column in columns:
+        cols.append(column)
+        
+    covmatrix = pd.DataFrame(data, columns = cols)
+    return covmatrix.cov()
+
+    corrmatrix = pd.DataFrame(data, columns = cols)   
+    return corrmatrix.corr()
