1ac893ccab5ffd4aa20c97c304b8dbb95b34c6e0
Jennifer Hoang <jennifer.anh.hoang@gmail.com>
2022-01-18 19:32:30 -0800

    Tests: add tests for plot_histogram

---
+++ tests/test_plot_histogram.py
@@ -0,0 +1,31 @@
+from EDAhelper.EDAhelper import plot_histogram
+import pandas as pd
+import pytest
+import altair as alt
+
+def df():
+    df = pd.DataFrame({
+        'A': [100, 142, 30, 40],
+        'B': [94, 55, 100, 120],
+        'C': [94, 68, 20, 17],
+        'D': [3.86, 4.05, 5.6, 1.0],
+        'E': [34, 92, 100, 200],
+        'name': ['Jennifer', 'Rowan', 'Steven', 'Vera']
+    })
+    return df
+
+def test_plot_histogram():
+    """Test plot_histogram on a dataframe."""
+    # Case 1: Test default settings and return
+    result = plot_histogram(df())
+    assert isinstance(result, alt.Chart, "Altair Chart object should be returned.")
+
+    # Case 2: Test erroneous inputs
+    with pytest.raises(Exception):
+        plot_histogram([1, 2, 3])
+
+    with pytest.raises(Exception):
+        plot_histogram(df(), columns = "A")
+
+    with pytest.raises(Exception):
+        plot_histogram(df(), num_bins = "ten")
