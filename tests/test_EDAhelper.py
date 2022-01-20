58d3eddd500dcd447813cfde84a94e731f9d484f
superNOVA <jinglu_cui@163.com>
2022-01-19 18:52:58 -0800

    Make full coverage test and document the test

--- tests/test_EDAhelper.py
+++ tests/test_EDAhelper.py
@@ -5,6 +5,31 @@ import pytest
 
 
 def expected_results(case):
+    """
+        Get expected data frame for the test.
+
+        Parameters
+        ----------
+        case : int, {2, 3, 4, 5, 6}
+            If case = 2, the expected data frame when `preprocess(file_path, method='mean')`.
+
+            If case = 3, the expected data frame when `preprocess(file_path, method='median')`.
+
+            If case = 4, the expected data frame when `preprocess(file_path, method='most_frequnet')`.
+
+            If case = 5, the expected data frame when `preprocess(file_path, method='constant')`.
+
+            If case = 6, the expected data frame when `preprocess(file_path, method='constant', fill_value=9999)`.
+
+        Returns
+        -------
+        pandas.DataFrame
+            The expected data frame for the test.
+
+        Examples
+        -------
+        >>> expected_results(2)
+        """
     if case == 2:
         expected_dict = {'col_1': [1.25, 1.0, 1.0, 3.0, 0.0],
                          'col_2': [1.0, 2.0, 8.0/3, 5.0, 8.0/3],
@@ -40,6 +65,28 @@ def expected_results(case):
 
 
 def test_preprocess():
+    """
+        Unit test of `preprocess(...)`.
+        Case 1: Test the type of result returned.
+        Case 2: Test method='mean'.
+        Case 3: Test method='median'.
+        Case 4: Test method='most_frequnet'.
+        Case 5, Test method='constant.
+        Case 6, Test method='constant', fill_value=9999.
+        Case 7: To test else branch.
+        Case 8: Test error input.
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+
+        Examples
+        -------
+        >>> test_preprocess()
+        """
+
     # Case 1: To test default settings and return
     file_path = 'https://raw.githubusercontent.com/pandas-dev/pandas/main/doc/data/titanic.csv'
     actual_df = preprocess(file_path)
@@ -75,7 +122,14 @@ def test_preprocess():
     test_result = actual_df.compare(expected_results(6))
     assert test_result.empty, "method='constant', fill_value=9999 applied incorrectly!"
 
-    # Case 7: To test inputs
+    # Case 7: To test else part
+    file_path2 = 'https://support.staffbase.com/hc/en-us/article_attachments/360009197031/username.csv'
+    actual_df = preprocess(file_path2, method='mean')
+    expected_df = pd.read_csv(file_path2)
+    test_result = actual_df.compare(expected_df)
+    assert test_result.empty, 'Data without missing value read incorrectly!'
+
+    # Case 8: To test inputs
     with pytest.raises(Exception):
         preprocess(5)
 
@@ -87,4 +141,3 @@ def test_preprocess():
 
     with pytest.raises(Exception):
         preprocess(file_path, index_col_1=1)
-
